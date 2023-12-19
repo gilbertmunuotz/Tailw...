56ce05f80080c9faedc26771b53af36eaaa1730f
gilbertmunuotz <officialgilbert24@gmail.com>
2023-12-19 13:36:15 +0300

    Done✔

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import Pilau from "./Images/Spicy-cooked-rice.jpg";
 
 function Home() {
     return (
         <div className='Home'>
-            <div className='body font-serif:Georgia bg-slate-500'>
+            <div className='body font-serif:Georgia bg-slate-200'>
                 <div className=''>
-                    <nav>
+                    <nav className='flex'>
                         <div className="mr-6">
-                            <h1 className='font-bold uppercase ml-4 border-b-2 border-gray-500'>
+                            <h1 className='font-bold uppercase ml-4 border-b-2 border-gray-500 md:text-2xl lg:text-2xl xl:text-2xl'>
                                 <a href="/">Food Ninja</a>
                             </h1>
                         </div>
-                        <ul>
+                        <ul className='flex space-x-4 justify-self-end ml-96 font-bold xl:ml-50'>
                             <li>
                                 <a href="#">
                                     <span>Home</span>
@@ -33,9 +34,9 @@ function Home() {
                 </div>
 
                 <main className='mx-6'>
-                    <div className='flex sm:justify-center lg:justify-end text-orange-400 mr-8 space-x-2'>
-                        <a href="#">Log in</a>
-                        <a href="#">Sign up</a>
+                    <div className='flex lg:justify-end text-orange-400 mr-8 space-x-2 md:justify-end 2xl:justify-right'>
+                        <a href="#" className='border-orange-500 border-2 rounded-full px-2 cursor-pointer'>Log in</a>
+                        <a href="#" className='border-orange-500 border-2 rounded-full px-2 cursor-pointer'>Sign up</a>
                     </div>
 
                     <header>
@@ -46,9 +47,54 @@ function Home() {
                     <div className=''>
                         <h4 className='border-b-2 border-gray-900 font-bold'>Latest Recipes</h4>
 
-                        <div className='my-4'>
+                        <div className='my-4 grid grid-cols-3 gap-6'>
                             {/* cards go here  */}
-                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl'>
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
+                                <img src={Pilau} alt="Pilau" className='w-full' />
+                                <div className='ml-2'>
+                                    <span className='font-bold'>5 Bean Chili Stew</span>
+                                    <span className='block'>Recipe by Mario</span>
+                                </div>
+                                <div className="absolute text-sm font-bold uppercase rounded-full p-2 top-0 bg-slate-600 ml-2 mt-2">25 Mins</div>
+                            </div>
+
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
+                                <img src={Pilau} alt="Pilau" className='w-full' />
+                                <div className='ml-2'>
+                                    <span className='font-bold'>5 Bean Chili Stew</span>
+                                    <span className='block'>Recipe by Mario</span>
+                                </div>
+                                <div className="absolute text-sm font-bold uppercase rounded-full p-2 top-0 bg-slate-600 ml-2 mt-2">25 Mins</div>
+                            </div>
+
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
+                                <img src={Pilau} alt="Pilau" className='w-full' />
+                                <div className='ml-2'>
+                                    <span className='font-bold'>5 Bean Chili Stew</span>
+                                    <span className='block'>Recipe by Mario</span>
+                                </div>
+                                <div className="absolute text-sm font-bold uppercase rounded-full p-2 top-0 bg-slate-600 ml-2 mt-2">25 Mins</div>
+                            </div>
+
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
+                                <img src={Pilau} alt="Pilau" className='w-full' />
+                                <div className='ml-2'>
+                                    <span className='font-bold'>5 Bean Chili Stew</span>
+                                    <span className='block'>Recipe by Mario</span>
+                                </div>
+                                <div className="absolute text-sm font-bold uppercase rounded-full p-2 top-0 bg-slate-600 ml-2 mt-2">25 Mins</div>
+                            </div>
+
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
+                                <img src={Pilau} alt="Pilau" className='w-full' />
+                                <div className='ml-2'>
+                                    <span className='font-bold'>5 Bean Chili Stew</span>
+                                    <span className='block'>Recipe by Mario</span>
+                                </div>
+                                <div className="absolute text-sm font-bold uppercase rounded-full p-2 top-0 bg-slate-600 ml-2 mt-2">25 Mins</div>
+                            </div>
+
+                            <div className='relative bg-white rounded-lg overflow-hidden shadow-xl hover:shadow-2xl cursor-pointer'>
                                 <img src={Pilau} alt="Pilau" className='w-full' />
                                 <div className='ml-2'>
                                     <span className='font-bold'>5 Bean Chili Stew</span>
@@ -66,7 +112,7 @@ function Home() {
                     </div>
 
                     <div className='flex justify-center'>
-                        <div>Load more</div>
+                        <div className='button tracking-wider'>Load more</div>
                     </div>
                 </main>
             </div>
